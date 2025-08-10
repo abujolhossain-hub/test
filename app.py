f2fe44e36aadecfbdd1628e7dad7867446307577
abujolhossain-hub <abujolhossain@gmail.com>
2025-08-11 00:26:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,8 @@ def delete_todo(todo_id):
     return jsonify({"message": "Deleted"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
